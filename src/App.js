992e8aa3c51197692dea12b34e9f279f22e11d7a
KesaviAravinthan <kesaviap@gmail.com>
2022-09-27 11:35:54 +0530

    daily Visualization line chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Overview from "./components/Overview";
+import DailyVisualization from "./components/DailyVisualization";
 
 function App() {
   return (
     <>
       <Overview />
+      <DailyVisualization/>
     </>
   );
 }
