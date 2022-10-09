c8f5e737db1829b54d9c50dca5e534a0850b38e2
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2022-10-09 18:53:28 +0530

    Setup temporary routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Overview from "./components/Overview";
 import DailyVisualization from "./components/DailyVisualization";
+import { Dashboard } from "./components/Dashboard";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Overview />
-      <DailyVisualization/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Overview/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dailyvisualization" element={<DailyVisualization/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,23 +3,25 @@ import {DailySesaonalChart} from './DailySeasonalChart';
 
 
 export function Dashboard(){
-
     return(
         <>
             <div className='row m-4'>
                 <div className='col-4'>
                     <div className='container shadow bg-white rounded-3'>
-                        <DailySesaonalChart/>
+                        {/* <DailySesaonalChart/> */}
+                        <h1>Hello</h1>
                     </div>
                 </div>
                 <div className='col-4'>
                     <div className='container shadow bg-white rounded-3'>
-                        <DailySesaonalChart/>
+                        {/* <DailySesaonalChart/> */}
+                        <h1>Hello</h1>
                     </div>
                 </div>
                 <div className='col-4'>
                     <div className='container shadow bg-white rounded-3'>
-                        <DailySesaonalChart/>
+                        {/* <DailySesaonalChart/> */}
+                        <h1>Hello</h1>
                     </div>
                 </div>
             </div>
