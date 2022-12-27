b0a3faefade421606cb7f44c633ff3bfc9f9b76d
NirmalSankalana <sankalana.nirmal@gmail.com>
2022-12-27 18:45:04 +0530

    issues in dashboard

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,29 @@
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
-export function Header(){
-    return(
-        <>
-        <Navbar variant="dark" className='shadow color-nav'>
-            <Container fluid>
-                <Navbar.Brand href="/">
-                    <img
-                    alt=""
-                    src="/cloudy.png"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                    />{' '}
-                    Forecaster
-                </Navbar.Brand>
-                <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-                <Nav.Link href="#about">About</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-        </>
-    )
+export function Header() {
+  return (
+    <>
+      <Navbar variant="dark" className="shadow color-nav">
+        <Container fluid>
+          <Navbar.Brand href="/" className="mx-5">
+            <img
+              alt=""
+              src="/cloudy.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Forecaster
+          </Navbar.Brand>
+          <Nav className="me-left mx-5">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
